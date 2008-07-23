2ccd45950e3a8c5cb056d58d488ab61e875785b4
Shay Arnett <shayarnett@gmail.com>
2008-07-23 15:49:04 -0400

    static and error changes

--- app/helpers/twictures_helper.rb
+++ app/helpers/twictures_helper.rb
@@ -1,23 +1,5 @@
 module Merb
     module TwicturesHelper
-      def embed_html(twicture)
-        snippet = <<-SNIP
-<div>
-<p><img src="http://twictur.es/images/#{twicture.image_path}" /></p>
-<p>image provided by #{link_to "twictur.es", 'http://twictur.es'} | original tweet #{link_to "here", twicture.twitter_url}</p>
-</div>
-        SNIP
-        tag :textarea, snippet
-      end
-      
-      def form_html
-        <<-html
-<form action="/new" method="post">
-<label for="twicture_twitter_url">Tweet URL</label>
-<input type="text" class="text" name="twicture[twitter_url]" value="" id="twicture_twitter_url"/>
-<button type="submit">Twicturlate</button>
-</form>
-        html
-      end
+
     end
 end
